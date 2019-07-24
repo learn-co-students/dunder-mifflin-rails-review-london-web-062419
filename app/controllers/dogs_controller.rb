bdb442cc9ce6df124735c7d31e04381ebafe594f
github username <github email address>
2019-07-24 21:15:52 +0100

    Done.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,63 @@
 class DogsController < ApplicationController
-end
+    # before_action :dog_id only: [:show, :edit, :update, :destroy]
+
+    def index 
+        @dogs = Dog.all
+    end 
+
+    def new 
+        @dog = Dog.new
+    end 
+
+    def show 
+      @dog = Dog.find(params[:id])
+    end
+
+    def create 
+        @dog = Dog.find(params[:id])
+        @dog = Dog.new(dog_params)
+        if @dog.valid?
+            @dog.save
+
+        redirect_to dog_path(@dog)
+
+        else 
+
+            render :new
+        end 
+    end 
+
+    def edit
+        @dog = Dog.find(params[:id])
+    end 
+
+    def update 
+        @dog = Dog.find(params[:id])
+        if @dog.update(dog_params)
+            redirect_to dog_path(@dog)
+        else 
+
+            render :edit
+        end 
+    end 
+
+    def destroy 
+        @dog = Dog.find(params[:id])
+        if @dog.destroy 
+            redirect_to dogs_path(@dog)
+        else 
+            render :edit 
+        end 
+    end 
+
+    private
+
+    # def dog_id
+    #     @dog = Dog.find(params[:id])
+    # end 
+
+    def dog_params
+        params.require(:dog).permit(:name, :breed, :age)
+    end 
+end 
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,35 @@
 Rails.application.routes.draw do
-  resources :dogs
-  resources :employees
+  # resources :dogs
+  # resources :employees
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #index 
+  get '/dogs', to: 'dogs#index'
+  #new
+  get "/dogs/new", to: "dogs#new", as: "new_dog"
+
+  #show
+  get "/dogs/:id", to: "dogs#show", as: "dog"
+  #create
+  post "/dogs", to: "dogs#create"
+  #edit
+  get "/dogs/:id/edit", to: "dogs#edit", as: "edit_dog"
+  #update
+  patch "/dogs/:id", to: "dogs#update"
+  #destroy/delete
+  delete "/dogs/:id", to: "dogs#destroy"
+  #same again for employees
+  #index
+  get '/employees', to: 'employees#index'
+  #new
+  get '/employees/new', to: 'employees#new', as: 'new_employee'
+  #show
+  get '/employees/:id', to: 'employees#show', as: "employee"
+  #create
+  post '/employees', to: 'employees#create'
+  #edit
+  get '/employees/:id/edit', to: 'employees#edit', as: 'edit_employee'
+  #update
+  patch '/employees/:id', to: 'employees#update'
+  #destroy
+  delete '/employees/:id', to: 'employees#destroy'
 end
